47871c3ea5f2b0b4459d88954593ee41071d4e7a
Hamza-Mahmood-3811 <hamzamahmood9151@gmail.com>
2023-09-25 17:30:34 -0400

    Outlining components and setting up page.jsx, layout.jsx

---
+++ components/Nav.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React, { useState, useEffect } from 'react'
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Nav = () => {
+  return (
+    <div>Nav</div>
+  )
+}
+
+export default Nav
